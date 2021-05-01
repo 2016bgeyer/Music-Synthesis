cb76906697b4f1942183e754dc7f840fdba93674
bengeyer <bengeyer@live.com>
2021-05-01 15:21:28 -0400

    updated model.py

--- ChordEstimation/model/model.py
+++ ChordEstimation/model/model.py
@@ -105,7 +105,7 @@ class MidiLSTM(BaseModel):
         melody_out = melody_out.transpose(0, 1)
         chord_out = chord_out.transpose(0, 1)
 
-        if MidiDataset.USE_SEQUENCE:
+        if MidiDataset.USE_SEQUENCE: # always False
             output = {
                 'melody_out': melody_out[:, -1],
                 'chord_out': chord_out[:, -1]
@@ -117,31 +117,6 @@ class MidiLSTM(BaseModel):
             }
 
         return output
-
-    def save_output(self, output, thresh=0.5):
-        melody_out = output['melody_out'].detach().numpy().argmax(dim=-1)
-        chord_out = output['chord_out'].detach().numpy() > thresh
-
-class SmallMidiLSTM(MidiLSTM):
-    """Smaller version of the Midi LSTM
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.hidden_network = nn.Sequential()
-        self.melody_classifier = nn.Sequential(
-            nn.Dropout(self.dropout),
-            nn.Linear(self.hidden_size, self.vocab_size)
-        )
-        if MidiDataset.USE_CHORD_ONEHOT:
-            self.chord_classifier = nn.Sequential(
-                nn.Dropout(self.dropout),
-                nn.Linear(self.hidden_size, MidiDataset.NUM_CHORDS)
-            )
-        else:
-            self.chord_classifier = nn.Sequential(
-                nn.Dropout(self.dropout),
-                nn.Linear(self.hidden_size, self.vocab_size)
-            )
 
 
 class BigMidiLSTM(MidiLSTM):
@@ -163,7 +138,7 @@ class BigMidiLSTM(MidiLSTM):
             nn.Dropout(self.dropout),
             nn.Linear(self.hidden_size//8, self.vocab_size),
         )
-        if MidiDataset.USE_CHORD_ONEHOT:
+        if MidiDataset.USE_CHORD_ONEHOT:    # always true
             self.chord_classifier = nn.Sequential(
                 nn.Dropout(self.dropout),
                 nn.Linear(self.hidden_size * self.scale, self.hidden_size//2),
@@ -191,21 +166,3 @@ class BigMidiLSTM(MidiLSTM):
                 nn.Dropout(self.dropout),
                 nn.Linear(self.hidden_size//8, self.vocab_size),
             )
-
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super(MnistModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
